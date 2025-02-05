f745e4dea504cbe2fca7ef91bcd5f638edddba8b
Franciscoffv <franffv0809@gmail.com>
2025-02-05 13:19:21 -0600

    TAREA1

---
+++ Tarea1/src/main/java/franciscotarea1/demo/Franciscotarea1Application.java
@@ -0,0 +1,13 @@
+package franciscotarea1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Franciscotarea1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Franciscotarea1Application.class, args);
+	}
+
+}
---
+++ Tarea1/src/test/java/franciscotarea1/demo/Franciscotarea1ApplicationTests.java
@@ -0,0 +1,13 @@
+package franciscotarea1.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Franciscotarea1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
